c5589c51bf568f1c40ce083b01b9cc7a49a5c532
Omkar Jagdale <omkarsj007@gmail.com>
2022-12-09 14:09:42 -0600

    stuff

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Card, ListGroup, Image } from "react-bootstrap";
 // import fs from 'fs'
-const Fs = require('fs') 
+const fs = require('fs') 
 
 const ProfileCard = (props) => {
   let path = "/img/" + props.user.id + ".jpg"
 
   
-  // if(!fs.existsSync(path)){
-  //   path = "/img/UD.jpg"
-  // }
-
+  fs.readFile('path', 'utf8' , (err, data) => {
+    if (err) {
+      path = "/img/UD.jpg"
+    }
+    console.log(data);
+  });
   return (
     <Card style={{ width: "12rem" }} border="light" className="shadows">
       <ListGroup variant="flush">
--- src/pages/BecomeHost.js
+++ src/pages/BecomeHost.js
@@ -10,8 +10,8 @@ import {
   Modal,
 } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
-import ImageUploading from "react-images-uploading";
+// import axios from "axios";
+// import ImageUploading from "react-images-uploading";
 const BecomeHost = () => {
   const [properties, setProperties] = useState([]);
 
