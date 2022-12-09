4787ebb2f0ab31fba3d465daad747b7e1bd01fe9
Omkar Jagdale <omkarsj007@gmail.com>
2022-12-09 15:58:55 -0600

    stuff

--- src/pages/Reservations.js
+++ src/pages/Reservations.js
@@ -1,75 +1,75 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Modal, Button } from "react-bootstrap";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Tooltip from "react-bootstrap/Tooltip";
 import { Link, useNavigate } from "react-router-dom";
 import "../components/styles/mystyles.css";
 
-const ReservationCard = (props) =>{
+const ReservationCard = (props) => {
 
-    const deleteReservation = () => {
-        const requestOptions = {
-            method: "DELETE",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            
-          };
-          fetch("http://localhost:3000/reservations?id=" + props.reservation.id, requestOptions)
-          .then((res) => res.json())
-          .then((data) => {
-            if (data.error) {
-              console.log(data.error);
-              props.setErrorMessage(data.error);
-              props.setError(true);
-            }
-            else{
-              props.setDelete(props.reservation.id)
-            }
-            
-          })
-            .catch(console.log);
+  const deleteReservation = () => {
+    const requestOptions = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
 
-        // fetch("http://localhost:3000/reservations")
-        //   .then((res) => res.json())
-        //   .then((data) => {
-        //     let a = data.filter((r) => r.customer_id == props.reservation.customer_id)
-        //     props.setReservation(a);
-        //   })
-        //   .catch(console.log);
-          
-    }
-    return(
-        <Container>
-            <div className="card-list mb-2 grow" style={{ height: "6rem" }}>
-                <img
-                className="cover rounded shadows"
-                src='https://media.istockphoto.com/id/534761709/photo/reserved-sign-on-restaurant-table.jpg?s=612x612&w=0&k=20&c=q3Jy3s-Cyqeq6MAVWC0vYPVIsNOdZHHxjJIBju1jCw0='
-                alt="background"
-                loading="lazy"
-                />
-                
-                <p
-                className="centered font text-shadow fs-3 ps-5 edit-link`"
-                style={{ color: "white" }}
-               
-                >
-                <pre style={{ color: "white" }}>
-                {props.reservation.listing_id}      {props.reservation.dateStart} - {props.reservation.dateEnd}
-                </pre>
-                
+    };
+    fetch("http://localhost:3000/reservations?id=" + props.reservation.id, requestOptions)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) {
+          console.log(data.error);
+          props.setErrorMessage(data.error);
+          props.setError(true);
+        }
+        else {
+          props.setDelete(props.reservation.id)
+        }
 
-                </p>
-                <OverlayTrigger placement="right" overlay={<Tooltip>Remove</Tooltip>}>
-                <i
-                    className="edit-link bi bi-trash btn-centered fs-2"
-                    style={{ color: "white" }}
-                    onClick={deleteReservation}
-                ></i>
-                </OverlayTrigger>
-            </div>
-        </Container>
-    )
+      })
+      .catch(console.log);
+
+    // fetch("http://localhost:3000/reservations")
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     let a = data.filter((r) => r.customer_id == props.reservation.customer_id)
+    //     props.setReservation(a);
+    //   })
+    //   .catch(console.log);
+
+  }
+  return (
+    <Container>
+      <div className="card-list mb-2 grow" style={{ height: "6rem" }}>
+        <img
+          className="cover rounded shadows"
+          src='https://media.istockphoto.com/id/534761709/photo/reserved-sign-on-restaurant-table.jpg?s=612x612&w=0&k=20&c=q3Jy3s-Cyqeq6MAVWC0vYPVIsNOdZHHxjJIBju1jCw0='
+          alt="background"
+          loading="lazy"
+        />
+
+        <p
+          className="centered font text-shadow fs-3 ps-5 edit-link`"
+          style={{ color: "white" }}
+
+        >
+          <pre style={{ color: "white" }}>
+            {props.reservation.listing_id}      {props.reservation.dateStart} - {props.reservation.dateEnd}
+          </pre>
+
+
+        </p>
+        <OverlayTrigger placement="right" overlay={<Tooltip>Remove</Tooltip>}>
+          <i
+            className="edit-link bi bi-trash btn-centered fs-2"
+            style={{ color: "white" }}
+            onClick={deleteReservation}
+          ></i>
+        </OverlayTrigger>
+      </div>
+    </Container>
+  )
 }
 
 const Reservations = (props) => {
@@ -97,37 +97,48 @@ const Reservations = (props) => {
     boxShadow: "rgb(0,0,0,0.44) 0px 5px 5px",
   };
 
-    useEffect(() => {
-        fetch("http://localhost:3000/reservations")
-          .then((res) => res.json())
-          .then((data) => {
-            let a = data.filter((r) => r.customer_id == info.id)
-            setReservations(a);
-          })
-          .catch(console.log);
-      }, [deleteRes]);
-    const navigate = useNavigate();
-    console.log(reservations)
-    console.log(info.id)
-    var list = reservations.map(x => {
-        });
+  useEffect(() => {
+    fetch("http://localhost:3000/reservations")
+      .then((res) => res.json())
+      .then((data) => {
+        let a = data.filter((r) => r.customer_id == info.id)
+        setReservations(a);
+      })
+      .catch(console.log);
+  }, [deleteRes]);
+  const navigate = useNavigate();
+  console.log(reservations)
+  console.log(info.id)
+  var list = reservations.map(x => {
+  });
 
-    return (
-        // reservations.map(fruit => <div
-        //     style={styles}></div>)
-        <Container>
-           <div className="m-5">
-            <p className="font fw-bold fs-1 m-5">Your Reservations</p>
-            <Row className="m-5">
-                {reservations.map((x, i) => (<ReservationCard key={i} reservation={x} setReservation={setReservations} setDelete={setDelete} setError={setError} setErrorMessage={setErrorMessage}/>))}  
-            </Row>
-        </div>
-        </Container>
-       
-        
-        
-        )
+  return (
+    // reservations.map(fruit => <div
+    //     style={styles}></div>)
+    <Container>
+      <div className="m-5">
+        <p className="font fw-bold fs-1 m-5">Your Reservations</p>
+        <Row className="m-5">
+          {reservations.map((x, i) => (<ReservationCard key={i} reservation={x} setReservation={setReservations} setDelete={setDelete} setError={setError} setErrorMessage={setErrorMessage} />))}
+        </Row>
+      </div>
 
-  };
-  
-  export default Reservations;
+      <Modal show={showError} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title> {errorMessage}</Modal.Title>
+        </Modal.Header>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            Okay
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </Container>
+
+
+
+  )
+
+};
+
+export default Reservations;
